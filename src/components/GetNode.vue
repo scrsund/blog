37a1c5d1acd247e1b6f910aaaf36ac5ae3338f18
scrsund <137787953+scrsund@users.noreply.github.com>
2024-11-20 12:05:42 +0100

    add script section

--- src/components/GetNode.vue
+++ src/components/GetNode.vue
@@ -9,5 +9,32 @@
       {{ node.value }}
     </template>
   </span>
-
 </template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: Array,
+      required: true,
+    }
+  },
+  methods: {
+    getClass(node){
+      return node.nodeType === 'text' ? 'inline' : '';
+    },
+    getMarks(node){
+      if(!node.marks) return [];
+      return node.marks.map((mark) => {
+        const markMap = {
+          bold: 'font-semibold',
+          italic: 'italic',
+          underline: 'underline',
+          strikethrough: 'line-through',
+        };
+        return markMap[mark.type] || '';
+      })
+    }
+  }
+}
+</script>
